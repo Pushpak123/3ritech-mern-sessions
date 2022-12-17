b4a463ea7ecf05df3fca1bc7873cab9bc600f961
rkitonlinetrainings <rkitonlinetrainings@gmail.com>
2022-12-17 18:16:18 +0530

    reactjs

---
+++ reactjs/session4/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return(
+        <div className="row navbar">
+            <div className="col-sm-12 text-sm-end">
+                <NavLink className="navlinks" activeClassName exact = "true" to="/"> Home </NavLink>
+                
+                <NavLink className="navlinks" activeClassName to="/event-handlers"> Event Handlers </NavLink>
+
+                <NavLink className="navlinks" activeClassName to="/form-validation"> Form Validation </NavLink>
+            </div>
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ reactjs/session4/src/pages/FormValidation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class FormValidation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailAddress: '', 
+            password: ''
+        }
+    }
+
+    render() {
+        return(
+        <div className="row">
+            <div className="col-sm-12 main__content">
+                <h3>Form Validation</h3>
+
+                <hr />
+
+                <form>
+                    <div className='form-group row'>
+                        <label className='col-4 text-sm-end'>Email Address</label>
+                        <div className='col-6'>
+                            <input type='text' name='emailAddress' value={this.state.emailAddress} placeholder='ex., john@gmail.com' className='form-control' />
+                        </div>
+                    </div>
+
+                    <div className='form-group row'>
+                        <label className='col-4 text-sm-end'>Password</label>
+                        <div className='col-6'>
+                            <input type='text' name='password' value={this.state.password} placeholder='ex., j0hN123' className='form-control' />
+                        </div>
+                    </div>
+
+                    <div className='form-group row'>
+                        <div className='col-6 offset-4'>
+                            <input type='submit' className='btn btn-primary' value='submit' />
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default FormValidation;

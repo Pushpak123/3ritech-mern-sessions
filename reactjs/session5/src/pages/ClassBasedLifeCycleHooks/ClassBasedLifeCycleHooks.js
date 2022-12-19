a4cf43f13e5803e791310c94a06f043c6f451cfe
rkitonlinetrainings <rkitonlinetrainings@gmail.com>
2022-12-19 10:27:56 +0530

    reactjs

---
+++ reactjs/session5/src/pages/ClassBasedLifeCycleHooks/ClassBasedLifeCycleHooks.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ClassBasedLifeCycleHooks = () => {
+    return(
+        <div className="row">
+            <div className="col-sm-12 main__content">
+                <h3>Class Based Life Cycle Hooks</h3>
+
+                <hr />
+
+                
+            </div>
+        </div>
+    )
+}
+
+export default ClassBasedLifeCycleHooks;
